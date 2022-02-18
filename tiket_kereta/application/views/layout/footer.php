dba384f94426b86d0d794eb682c6291263e730af
risma261 <syafeirisma@students.unnes.ac.id>
2022-02-18 20:21:23 +0700

    add feature A

---
+++ tiket_kereta/application/views/layout/footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?= $title; ?></title>
+	
+	<link href="<?= base_url('assets/bg.png'); ?>fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+
+</nav>
